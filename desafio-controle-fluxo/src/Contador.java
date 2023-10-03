c919b50f763c82a5f6194346ad0d202fc89f6650
vickats <victoriaktsuha@outlook.com>
2023-10-03 18:34:25 -0300

    fix - try Scanner

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
 
-        try {
-            int[] qntdIteracoes = {parametroUm - parametroDois};
-            //chamando método contendo a lógica de contagem
-            contar(parametroUm, parametroDois, qntdIteracoes);
-        }catch(ParametrosInvalidosExeception e){
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            try {
+                int[] qntdIteracoes = {parametroUm - parametroDois};
+                //chamando método contendo a lógica de contagem
+                contar(parametroUm, parametroDois, qntdIteracoes);
+            }catch(ParametrosInvalidosExeception e){
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            }
         }
     }
     static void contar(int parametroUm, int parametroDois, int[] qntdIteracoes) throws ParametrosInvalidosExeception{
